30ed0b0a18c1e13b4825da666716962b54d400c5
haedirasso-star <haedirasso@gmail.com>
2026-02-05 21:58:15 +0300

    Delete lib directory

--- lib/constants.dart
+++
@@ -1,9 +0,0 @@
-class AppAssets {
-  // حقوق المطور والروابط الرسمية
-  static const String telegramChannel = "https://t.me/O_2828";
-  static const String whatsappSupport = "https://wa.me/9647714415816";
-
-  // مفاتيح السيرفرات والـ API
-  static const String tmdbApiKey = "ضع_مفتاحك_هنا"; // لجلب صور الأفلام
-  static const String movieStreamServer = "https://vidsrc.to/embed/movie/"; // سيرفر 1080p
-}
