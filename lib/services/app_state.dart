0b38fcc4051bcb2c13654514940d081bd4e58f9b
haedirasso-star <haedirasso@gmail.com>
2026-06-12 11:10:45 -0700

    Delete lib directory

--- lib/services/app_state.dart
+++
@@ -1,4 +0,0 @@
-part of '../main.dart';
-// app_state.dart — محتوى نُقل إلى constants.dart (AppState class)
-// هذا الملف يبقى فارغاً للتوافق مع نظام part/part-of
-// جميع منطق AppState موجود في lib/core/constants.dart
